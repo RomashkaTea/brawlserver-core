ef8cdfc99737786de800e13270cd0de087c289ab
Romashka <80623570+RomashkaTea@users.noreply.github.com>
2022-04-22 21:30:35 +0300

    added readBoolean and writeBoolean!

--- datastream/bytestream.py
+++ datastream/bytestream.py
@@ -1,9 +1,10 @@
 
 class ByteStream:
     def __init__(self, buffer=b''):
-        self.buffer = buffer
+        self.buffer = bytearray(buffer)
         self.offset = 0
         self.bitIdx = 0
+        self.currentByte = 0
     
     def readBytes(self, length):
         array = self.buffer[self.offset:self.offset+length]
@@ -11,18 +12,29 @@ class ByteStream:
         return array
     
     def readByte(self):
-        return int.from_bytes(self.readBytes(1), "big")
+        return int.from_bytes(self.readBytes(1), "big", signed=True)
 
     def readInt(self):
         return int.from_bytes(self.readBytes(4), "big")
-    
+
+    def readBoolean(self):
+        if self.bitIdx == 0:
+            self.currentByte = self.buffer[self.offset]
+        
+        result = ((1 << self.bitIdx) & self.currentByte) != 0
+        self.bitIdx = (self.bitIdx + 1) & 7
+        return result
+
     def readString(self, max=900000):
         length = self.readInt()
         if length < 0 and length > max:
             return None
-        return self.readBytes(length).decode("utf-8")
+        string_bytes = self.buffer[self.offset:self.offset+length]
+        self.offset += length
+        return string_bytes.decode("utf-8")
 
     def writeBytes(self, array):
+        self.bitIdx = 0
         self.buffer += array
         self.offset += len(array)
     
@@ -36,6 +48,15 @@ class ByteStream:
         array = value.encode("utf-8")
         self.writeInt(len(array))
         self.writeBytes(array)
+
+    def writeBoolean(self, value:bool):
+        if self.bitIdx == 0:
+            self.buffer += bytearray(1) # ensure
+        
+        if value:
+            self.buffer[self.offset] |= (1 << self.bitIdx)
+ 
+        self.bitIdx = (self.bitIdx + 1) & 7
 
     def writeVInt(self, value:int):
         pass # а сами сделайте.
